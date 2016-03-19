8b0694315e7c0892d2656db7f63f947202362b93
haveyoumetjess <jessmireau@Jesss-MBP.fios-router.home>
2016-03-18 23:10:12 -0400

    Syncing repository

---
+++ jess_mireau_week07/js/script.js
@@ -0,0 +1,11 @@
+// console.log('script loaded');
+
+$('#coralButton').click(function(){
+	// console.log('button clicked');
+	favicon.change("images/favicon-coral-32x32.png");
+});
+
+$('#tealButton').click(function(){
+	// console.log('button clicked');
+	favicon.change("images/favicon-teal-32x32.png");
+});
